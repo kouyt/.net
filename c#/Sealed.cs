0180ba50dcc8543e9fcf05f5a1695e5876720c82
Atsukon Lee <Aisuko@users.noreply.github.com>
2016-05-22 22:18:28 +0800

    修改分号错误

--- c#/Sealed.cs
+++ c#/Sealed.cs
@@ -3,12 +3,26 @@
 //sealed 当对一个类应用 sealed 修饰符时，此修饰符会阻止其他类从该类继承.
 //还可以在重写基类中的虚方法或虚属性的方法或属性上使用 sealed 修饰符。 这将使您能够允许类从您的类继承，并防止它们重写特定的虚方法或虚属性。
 
-using System.
 
 public sealed class NameA
 {
-    System.Console.WriteLine("hello world");
-    
+   public  string phone { get; set; }
+
+   public string print();
 }
 
+public interface NameC
+{
+    string classes { get; set; }
+
+    public string printIn();
+}
+
+public class NameB:NameC
+{
+    void WriteOut()
+    {
+        
+    }
+}
 
