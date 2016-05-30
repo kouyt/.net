448ac07044267996a08815ca2f09896fe58be277
Atsukon Lee <Aisuko@users.noreply.github.com>
2016-05-30 22:06:07 +0800

    .NET命名空间

---
+++ c#/ClassRepository.cs
@@ -0,0 +1,17 @@
+//.net类库：.NET Framework 类库是一个库的类、 接口和值类型提供对系统功能的访问权限。 它是.NET framework 构建应用程序、 组件和控件的基础
+//- 命名空间
+//Microsoft.Win32:处理操作系统引发的事件，操作系统注册表
+//System：包含用于定义常用值和引用数据类型、事件和事件处理程序、接口、特性以及处理异常的基础类和基类。
+//System.Activities:包含（windows workflow foundation windows基础工作流）处理活动所需要的所有类。
+//System.Data :构成ADO.NET体系结构和ADO.NET数据提供程序。
+using System.Diagnostics;   //可以与系统进程、事件日志和性能计数器之间进行交互。--子命名空间：与代码分析工具进行交互扩展对应用程序监控和检测的设计时支持，使用 Windows 事件跟踪 (ETW) 跟踪子系统来记录事件数据，在事件日志中进行读取和写入，收集性能数据，以及读取和写入调试符号信息。
+using System.Dynamic;       //提供类和支持动态语言运行时的接口
+using System.Drawing;       //支持基本GDI+ 图形功能
+using System.IO;
+using System.Linq;
+using System.Reflection;      //动态编程
+using System.Resources;     //能让开发人员创建、存储和管理应用程序的区域性特定资源。
+using System.Runtime;       //支持应用程序与公共语言运行时的交互，支持应用程序数据缓存、高级异常处理、应用程序域内的应用程序激活、COM 互操作、分布式应用程序、序列化和反序列化，以及版本控制等功能。 另外，其他子命名空间还能让编译器编写人员指定特性来影响公共语言运行时的运行时行为，在一组代码和其他依赖它的代码之间定义可靠性协定，以及实现 Windows Communication Foundation (WCF) 的持久性提供程序。
+using System.Threading;     //System.Threading 命名空间包含启用多线程编程的类型。 还有一个子命名空间提供可简化并发和异步代码编写工作的类型。
+using System.Web;               //ASP.NET   、JSON序列化、AJAX功能并入.net
+using System.Windows;       //winform
